d91ca9d5b9effa30360f1a552f175c9c26c52f6a
hxcde <30338980+hxcde@users.noreply.github.com>
2023-04-08 21:39:51 +0200

    Create zip_folder_daily.sh

---
+++ zip_folder_daily.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directory to be zipped (e.g., /path/to/folder)
+SOURCE_DIR="/path/to/folder"
+
+# Directory where the zipped files will be stored (e.g., /path/to/archive)
+ARCHIVE_DIR="/path/to/archive"
+
+# Create a timestamp in the format YYYY-MM-DD
+DATE=$(date +%Y-%m-%d)
+
+# Create the zipped archive and append the date
+zip -r "${ARCHIVE_DIR}/folder_${DATE}.zip" "${SOURCE_DIR}"
+
+# Delete files older than 14 days
+find "${ARCHIVE_DIR}" -name "folder_*.zip" -mtime +13 -exec rm {} \;
