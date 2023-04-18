7890f961361402445e6abb2b41807af116c4ab3a
Yann Mölle <hxcde@noreply.localhost>
2023-04-18 15:32:18 +0200

    „install.sh“ ändern

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 cd /opt/
 git clone https://git.moelle.space/hxcde/folderarchive.git
-0 13 * * * sh /opt/folderarchive/zip_folder_daily.sh
+chmod +x /folderarchive/zip_folder_daily.sh
+crontab -l > mycron
+echo "0 13 * * * sh /opt/folderarchive/zip_folder_daily.sh" >> mycron
+crontab mycron
+rm mycron
