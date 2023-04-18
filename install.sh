42c50a8aa421f6e5211166b48101708726ff28e4
Yann Mölle <hxcde@noreply.localhost>
2023-04-18 19:46:56 +0200

    „install.sh“ ändern

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 cd /opt/
-git clone https://git.moelle.space/hxcde/folderarchive.git
-chmod +x /folderarchive/zip_folder_daily.sh
-crontab -l > mycron
-echo "0 13 * * * sh /opt/folderarchive/zip_folder_daily.sh" >> mycron
-crontab mycron
-rm mycron
+echo "#!/bin/bash" > zip_folder_daily.sh
+echo "# Enter the directory to be zipped (e.g., /path/to/folder)" >> zip_folder_daily.sh
+read path
+echo "SOURCE_DIR=" '$path' " " >> zip_folder_daily.sh
+echo "# Enter the directory where the zipped files will be stored (e.g., /path/to/archive)" >> zip_folder_daily.sh
+read archive
+echo "ARCHIVE_DIR=" '$archive' "" >> zip_folder_daily.sh
+echo "DATE=$(date +%Y-%m-%d)" >> zip_folder_daily.sh
+echo "zip -r "${ARCHIVE_DIR}/folder_${DATE}.zip" "${SOURCE_DIR}"" >> zip_folder_daily.sh
+echo "find "${ARCHIVE_DIR}" -name "folder_*.zip" -mtime +13 -exec rm {} \;" >> zip_folder_daily.sh
+
+
+#crontab -l > mycron
+#echo "0 13 * * * sh /opt/folderarchive/zip_folder_daily.sh" >> mycron
+#crontab mycron
+#rm mycron
