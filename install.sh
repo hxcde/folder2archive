979dbb84f3b134a0da18f7ed9bae4ffba23e9468
Yann Mölle <hxcde@noreply.localhost>
2023-04-18 19:51:53 +0200

    „install.sh“ ändern

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ echo "#!/bin/bash" > zip_folder_daily.sh
 echo "#Enter the directory to be zipped (e.g., /path/to/folder)" >> zip_folder_daily.sh
 echo "Enter the directory to be zipped (e.g., /path/to/folder)"
 read path
-echo "SOURCE_DIR=" $path " " >> zip_folder_daily.sh
+echo "SOURCE_DIR=""$path""" >> zip_folder_daily.sh
 echo "# Enter the directory where the zipped files will be stored (e.g., /path/to/archive)" >> zip_folder_daily.sh
 echo "Enter the directory where the zipped files will be stored (e.g., /path/to/archive)"
 read archive
-echo "ARCHIVE_DIR=" $archive "" >> zip_folder_daily.sh
+echo "ARCHIVE_DIR=""$archive""" >> zip_folder_daily.sh
 echo "DATE=$(date +%Y-%m-%d)" >> zip_folder_daily.sh
 echo "zip -r "${ARCHIVE_DIR}/folder_${DATE}.zip" "${SOURCE_DIR}"" >> zip_folder_daily.sh
 echo "find "${ARCHIVE_DIR}" -name "folder_*.zip" -mtime +13 -exec rm {} \;" >> zip_folder_daily.sh
